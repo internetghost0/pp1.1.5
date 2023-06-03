e83f4a08352299521aed08c38cfbf59acba331aa
zer0 <internetghost0@protonmail.com>
2023-06-03 11:21:58 +0000

    add annotations to fields

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,12 +1,26 @@
 package jm.task.core.jdbc.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "lastname")
     private String lastName;
 
+    @Column(name = "age")
     private Byte age;
 
     public User() {
