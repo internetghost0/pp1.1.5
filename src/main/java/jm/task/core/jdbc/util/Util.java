cadf52c67b3343dccad419943fdb2bac3bd5d5ec
zer0 <internetghost0@protonmail.com>
2023-06-03 12:15:20 +0000

    now it works, forget to add `Class.forName`

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,17 +3,19 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
 import jm.task.core.jdbc.model.User;
 
 public class Util {
     private final static String URL = "jdbc:postgresql://localhost:5432/firstdb";
-    private final static String USER = "zer0";
+    private final static String USERNAME = "zer0";
     private final static String PASSWORD = "toor";
     private final static String DRIVER = "org.postgresql.Driver";
 
@@ -26,7 +28,7 @@ public class Util {
             System.err.println(e);
         }
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
@@ -35,18 +37,25 @@ public class Util {
 
     // dont quite understand the task, so implemented 2 ways of configuration
     public static SessionFactory getSessionFactory() {
+        try {
+            Class.forName(DRIVER);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
         if (sessionFactory == null) { 
-            Configuration configuration = new Configuration(); 
-            configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver"); 
-            configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/mydatabase"); 
-            configuration.setProperty("hibernate.connection.username", "myusername"); 
-            configuration.setProperty("hibernate.connection.password", "mypassword"); 
-            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect"); 
-            configuration.setProperty("hibernate.show_sql", "true"); 
-             
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder() 
-                    .applySettings(configuration.getProperties()).build(); 
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry); 
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, DRIVER);
+            settings.put(Environment.URL, URL);
+            settings.put(Environment.USER, USERNAME);
+            settings.put(Environment.PASS, PASSWORD);
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         } 
         return sessionFactory;
     }
